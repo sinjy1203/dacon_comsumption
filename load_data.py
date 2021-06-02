d5ee94552e19e7d37855a958968d0870920049ba
sinjy <sinjy1203@gmail.com>
2021-06-02 21:01:03 +0900

    commit

--- load_data.py
+++ load_data.py
@@ -6,3 +6,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data_dir = Path("data")
+train_dir = data_dir / "train.csv"
+test_dir = data_dir / "test.csv"
+train = pd.read_csv(train_dir, encoding='cp949')
+test = pd.read_csv(test_dir, encoding='cp949')
+
+##
+building_num = 60
+total_time = len(train) // building_num
+
+##
+
